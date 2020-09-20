a084e2bca4ddd8261905bfac5299795be06ff938
ivanvertylo <ivanvertylo@gmail.com>
2020-09-20 16:27:37 +0300

    First commit

---
+++ src/main/java/Servlet.java
@@ -0,0 +1,12 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet("/123")
+public class Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().print("LOL");
+    }
+}
